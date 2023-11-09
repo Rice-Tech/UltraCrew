911db73b3228efac13441cd7b2f62b995ad7db4d
Matt Rice <matthew.rice.d@gmail.com>
2023-11-09 18:23:20 -0500

    Defined models in runner and implemented corresponding forms, not working yet

--- ultracrew/runner/models.py
+++ ultracrew/runner/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+    
+class Race(models.Model):
+    name = models.CharField(max_length=64)
+    date = models.DateField()
+
+class AidStation(models.Model):
+    name = models.CharField(max_length=64)
+    distance = models.FloatField()
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+class Checkpoints(models.Model):
+    time = models.TimeField()
+    runner = models.ForeignKey(User, on_delete=models.CASCADE)
+    station = models.ForeignKey(AidStation, on_delete=models.CASCADE)
+
+class RaceRegistration(models.Model):
+    participant = models.ForeignKey(User, on_delete=models.CASCADE, related_name="participant")
+    crew = models.ManyToManyField(User, related_name="crew")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    minPace = models.DurationField() #pace in minutes per mile
+    maxPace = models.DurationField()
+    goalTime = models.DurationField() 
--- ultracrew/runner/views.py
+++ ultracrew/runner/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView
 from django import forms
-from django.forms import formset_factory
+from django.forms import ModelForm, modelformset_factory
+from .models import Race, AidStation, RaceRegistration
 
-class StationForm(forms.Form):
-    name = forms.CharField(max_length=64)
-    distance = forms.FloatField()
-
-StationForms = formset_factory(StationForm, extra=7, max_num=20)
 
 #https://www.brennantymrak.com/articles/django-dynamic-formsets-javascript
 
-class RaceForm(forms.Form):
-    name = forms.CharField(max_length=64)
-    date = forms.DateField()
-    stations = StationForms(prefix='station')
-    #formset_factory(stationForm, extra=1, max_num=20)
+class RaceForm(ModelForm):
+    class Meta:
+        model = Race
+        fields = ['name', 'date']
+
+class RaceRegistrationForm(ModelForm):
+    class Meta:
+        model = RaceRegistration
+        fields = ['minPace', 'maxPace', 'goalTime', 'crew']
 
 
-# Create your views here.
+class AidStationForm(ModelForm):
+    class Meta:
+        model: AidStation
+        fields: ['name', 'distance']
+
+StationForms = modelformset_factory(AidStation, AidStationForm, fields= ['name', 'distance'], extra=1, max_num=20)
+
+class FullForm(forms.Form):
+    race = RaceForm()
+    raceRegistration = RaceRegistrationForm()
+    stations = StationForms(prefix="station")
+
+# views
 
 def dashboard(request):
     return render(request, "runner/dashboard.html")
@@ -28,12 +41,12 @@ def runnerPage(request, name):
 def addRace(request):
     if request.method == "POST":
         #Validate data and rearange to desired format
-        raceform = RaceForm(request.POST, prefix='raceform')
+        raceform = FullForm(request.POST, prefix='raceform')
         if raceform.is_valid():
             return redirect("/runner/" + raceform.name)
         
     else:
-        raceform = RaceForm(prefix='raceform')    
+        raceform = FullForm(prefix='raceform')    
 
     return render(request, "runner/addRace.html", {"raceform":raceform})
 
